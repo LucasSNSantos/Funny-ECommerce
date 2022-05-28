47a8ac5118b1943500f2c7c5dc2458e77ba3ffc7
Lucas Souza <souzacontato18@gmail.com>
2022-05-28 01:55:53 -0300

    readme and 1th commit

---
+++ FunnyECommerce/Models/Client.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+public class Client
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public float Balance { get; set; }
+    public string Genre { get; set; }
+    public int UserID { get; set; }
+    public User User { get; set; }
+
+}
---
+++ FunnyECommerce/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+public class Product
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public float Price { get; set; }
+    public string Description { get; set; }
+    public int UserID { get; set; }
+    public User User { get; set; }
+}
